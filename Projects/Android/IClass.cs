5ef5867424796ac1586f5e2c17c7935ac1c823b3
LorelaiDavis <Lorelai.Davis@ResonantSciences.com>
2025-07-07 09:46:45 -0400

    Initial commit

---
+++ Projects/Android/IClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAZR_PointCRep
+{
+    internal interface IClass
+    {
+        void Initialize();
+        void Step();
+        void Shutdown();
+    }
+}
